d651261aba297f2505475d70fbeb8fd0424e6758
shriramcs <shriramcs@gmail.com>
2020-07-26 21:04:34 +0530

    updated angular version 7 to 10

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# NgCoursesUi
+# NgCourses
 
 Courses list developed using angular framework
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
+## Live
+
+[DEMO](https://ng-courses.netlify.app/)
 
 ## Development server
 
@@ -27,7 +29,3 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
-
-## Live demo
-
-DEMO (https://ng-courses.netlify.app/)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   {
     path: "courses",
-    loadChildren: "./courses/courses.module#CoursesModule",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
   },
 ];
 
